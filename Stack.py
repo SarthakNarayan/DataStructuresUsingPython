f99e30b7a83c7d6cac51da079339b625be10b069
arksn21 <56257322+arksn21@users.noreply.github.com>
2020-03-22 14:27:15 +0530

    Checking Balanced paranthesis

--- Stack.py
+++ Stack.py
@@ -8,7 +8,7 @@ class Stack():
     def push(self,item):
         self.items.append(item)
     def pop(self):
-        self.items.pop()
+        return self.items.pop()
     def get_stack(self):
         return self.items
     def is_empty(self):
@@ -32,3 +32,38 @@ s.pop()
 print(s.get_stack())
 print(s.is_empty())
 print(s.peek())
+
+
+# Checking balanced parenthesis
+def checker(p1,p2):
+    if p1 == '(' and p2 == ')':
+        return True
+    elif p1 == '{' and p2 == '}':
+        return True
+    elif p1 == '[' and p2 == ']':
+        return True
+    else:
+        return False
+
+def check_balanced_paranthesis(parenthesis):
+    st = Stack()
+    Balanced = True
+    for i in parenthesis:
+        if i in '{[(':
+            st.push(i)
+        else:
+            if st.is_empty():
+                Balanced = False
+                break
+            else:
+                top = st.pop()
+                Balanced = checker(top,i)
+                if not Balanced:
+                    break
+
+    if Balanced and st.is_empty():
+        return True
+    else:
+        return False
+
+print(check_balanced_paranthesis('()({})'))
